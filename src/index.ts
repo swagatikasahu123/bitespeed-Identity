5bdc90b4271f55d8f6d5ad82d08fcca918b04920
swagatikasahu123 <155278112+swagatikasahu123@users.noreply.github.com>
2025-05-20 23:25:30 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import identifyRoute from './routes/identifyRoute';
+import { createTableQuery } from './db/contact-table-creation';
+import { pool } from './db/index';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+async function createContactTable() {
+  try {
+    await pool.query(createTableQuery);
+    console.log('Contact table initialized successfully.');
+  } catch (err) {
+    console.error('Error initializing Contact table:', err);
+  } 
+}
+
+createContactTable();
 
 app.use(express.json());
 
@@ -13,5 +26,5 @@ app.use(express.json());
 app.use('/', identifyRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(Server running on port ${PORT});
 });
