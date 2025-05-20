5ba39c8515f70d7be290ed33ca9c1fce66403107
swagatikasahu123 <155278112+swagatikasahu123@users.noreply.github.com>
2025-05-20 22:55:00 +0530

    Update contact-table-creation.ts

--- src/db/contact-table-creation.ts
+++ src/db/contact-table-creation.ts
@@ -1,1 +1,12 @@
-
+export const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS Contact (
+    id SERIAL PRIMARY KEY,
+    phoneNumber VARCHAR(255),
+    email VARCHAR(255),
+    linkedId INTEGER REFERENCES Contact(id),
+    linkPrecedence VARCHAR(10) CHECK (linkPrecedence IN ('primary', 'secondary')) NOT NULL DEFAULT 'primary',
+    createdAt TIMESTAMP DEFAULT NOW(),
+    updatedAt TIMESTAMP DEFAULT NOW(),
+    deletedAt TIMESTAMP
+  );
+`;
